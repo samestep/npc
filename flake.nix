63fb6259413b67a49c7a753887afe214498537ce
Sam Estep <sam@samestep.com>
2025-10-11 22:58:40 -0400

    Provide Nix package via crane

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,6 +13,7 @@
       self,
       nixpkgs,
       flake-utils,
+      crane,
       rust-overlay,
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -21,8 +23,19 @@
           inherit system;
           overlays = [ (import rust-overlay) ];
         };
+        craneLib = crane.mkLib pkgs;
+        commonArgs = {
+          src = ./.;
+          strictDeps = true;
+        };
       in
       {
+        packages.default = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          }
+        );
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nixfmt
